c7abf9287aca879149db1b50629a9fe1451cad61
prlahoti <35039032+prlahoti@users.noreply.github.com>
2018-01-04 18:05:38 +0530

    Update index.php

--- index.php
+++ index.php
@@ -10,10 +10,10 @@ $n1->get();
 <form method="get" action="opt.php">
 Id: <input type="text" name="id"><br><br>
 Name: <input type="text" name="name"><br><br>
-<button type="submit" name="select" value="select" onclick="select();">Select</button>
-<button type="submit" name="insert" value="insert" onclick="insert();">Insert</button>
-<button type="submit" name="update" value="update" onclick="update();">Update</button>
-<button type="submit" name="delete" value="delete" onclick="del();">Delete</button>
+<button type="submit" name="select">Select</button>
+<button type="submit" name="insert">Insert</button>
+<button type="submit" name="update">Update</button>
+<button type="submit" name="delete">Delete</button>
 </form> 
 
 </body>
